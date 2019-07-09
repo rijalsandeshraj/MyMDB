6ba5357c0f262bf9033c414884db8d806752a413
Sandesh Rijal <sandesh.rijal@aayulogic.com>
2019-07-09 18:13:14 +0545

    Adjustment related to votes.

--- django/core/models.py
+++ django/core/models.py
@@ -103,6 +103,16 @@ class Role(models.Model):
         return '{} {} {}'.format(self.movie_id, self.person_id, self.name)
 
 
+class VoteManager(models.Manager):
+    def get_vote_or_unsaved_blank_vote(self, movie, user):
+        try:
+            return Vote.objects.get(movie=movie,
+                                    user=user)
+        except Vote.DoesNotExist:
+            return Vote(movie=movie,
+                        user=user)
+
+
 class Vote(models.Model):
     UP = 1
     DOWN = -1
@@ -115,6 +125,8 @@ class Vote(models.Model):
     user = models.ForeignKey(USER, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     voted_on = models.DateTimeField(auto_now=True)
+
+    objects = VoteManager()
 
     class Meta:
         unique_together = ('user', 'movie')
--- django/core/views.py
+++ django/core/views.py
@@ -1,5 +1,8 @@
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
-from .models import Person, Movie
+
+from django.core.forms import VoteForm
+from .models import Person, Movie, Vote
 
 
 class PersonDetailView(DetailView):
@@ -12,3 +15,24 @@ class MovieListView(ListView):
 
 class MovieDetailView(DetailView):
     queryset = Movie.objects.all_with_related_persons()
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            vote = Vote.objects.get_vote_or_unsaved_blank_vote(
+                movie=self.object,
+                user=self.request.user
+            )
+
+            if vote.id:
+                vote_form_url = reverse('core:update_vote', kwargs={'movie_id': vote.movie.id,
+                                                                    'pk': vote.id})
+            else:
+                vote_form_url = (reverse('core:create_view',
+                                         kwargs={'movie_id': self.object.id}))
+
+                vote_form = VoteForm(isinstance=vote)
+                ctx['vote_form'] = vote_form
+                ctx['vote_form_url'] = vote_form_url
+            return ctx
